cc8b84913957d18bfc4afb84badacf6e858e6ad5
GBobello <bobello.gabriel@gmail.com>
2024-02-13 08:26:21 -0300

    Terminando a segunda aula, desenvolvido tela de exibição do produto assim como os controles para que seja contabilizado a quantidade de produtos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,38 @@
 import { CategoryButton } from '@/components/category-button'
 import { Header } from '@/components/header'
-import { View, FlatList } from 'react-native'
-import { CATEGORIES } from '@/utils/data/products'
-import { useState } from 'react'
+import { View, FlatList, SectionList, Text } from 'react-native'
+import { Link } from 'expo-router'
+import { CATEGORIES, MENU } from '@/utils/data/products'
+import { useState, useRef } from 'react'
+import { Product } from '@/components/product'
+import { useCartStore } from '@/stores/cart-store'
  
 export default function Home() {
+    const cartStore = useCartStore()
 
     const [category, setCategory] = useState(CATEGORIES[0])
+    
+    const sectionListRef = useRef<SectionList>(null)
+
+    const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0) 
 
     function handleCategorySelected(selectedCategory: string) {
         setCategory(selectedCategory)
+
+        const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+        if (sectionListRef.current){
+            sectionListRef.current.scrollToLocation({
+                animated: true,
+                sectionIndex,
+                itemIndex: 0,
+            })
+        }
     }
 
     return(
         <View className='flex-1 pt-8'>
-            <Header title='Faça seu pedido' cartQuantityItems={1} />
+            <Header title='Faça seu pedido' cartQuantityItems={cartQuantityItems} />
 
             <FlatList 
                 data={CATEGORIES}
@@ -30,6 +48,26 @@ export default function Home() {
                 className='max-h-10 mt-5'
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
+
+            <SectionList 
+                sections={MENU}
+                ref={sectionListRef}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false}
+                renderItem={({ item }) => (
+                    <Link href={`/product/${item.id}`} asChild >
+                        <Product data={item} />
+                    </Link>
+                )}
+                renderSectionHeader={({section: { title }}) => (
+                    <Text className='text-xl text-white font-heading mt-8 mb-3' >
+                        {title}
+                    </Text>
+                )}
+                className='flex-1 p-5'
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
             />
         </View>
     )
---
+++ src/components/product.tsx
@@ -0,0 +1,35 @@
+import { Image, ImageProps, Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import { forwardRef  } from 'react'
+
+type ProductDataProps = {
+    title: string
+    description: string
+    thumbnail: ImageProps
+}
+type ProductProps = TouchableOpacityProps & {
+    data: ProductDataProps
+}
+
+export const Product = forwardRef<TouchableOpacity, ProductProps> (({ data, ...rest }, ref) => {
+    return(
+        <TouchableOpacity 
+            className="w-full flex-row items-center pb-4" 
+            ref={ref}
+            {...rest}
+        >
+            <Image 
+                source={data.thumbnail} 
+                className="w-20 h-20 rounded-md" 
+            />
+
+            <View className="flex-1 ml-3" >
+                <Text className="text-slate-100 font-subtitle text-base flex-1" >
+                    {data.title}
+                </Text>
+                <Text className="text-slate-400 text-xs leading-5 mt-0.5" >
+                    {data.description}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    )
+})
