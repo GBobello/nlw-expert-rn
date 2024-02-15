5c1448bd6d7f5340edd1c55f72b772b6041489e4
GBobello <bobello.gabriel@gmail.com>
2024-02-15 07:54:30 -0300

    Finishing the application: implementing send cart as message with WhatApp API and some ajusts in features of system!

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,18 +1,67 @@
 import { Header } from "@/components/header";
-import { ScrollView, Text, View } from "react-native";
+import { Alert, ScrollView, Text, View, Linking } from "react-native";
 import { Product } from "@/components/product";
-import { useCartStore } from "@/stores/cart-store";
+import { ProductCartProps, useCartStore } from "@/stores/cart-store";
 import { formatCurrency } from "@/utils/functions/format-curency";
 import { Input } from "@/components/input";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Button } from "@/components/button";
 import { Feather } from "@expo/vector-icons";
 import { LinkButton } from "@/components/link-button";
+import { useState } from "react";
+import { useNavigation } from "expo-router";
+
+const PHONE_NUMBER = '5547999950797'
 
 export default function Cart() {
+    const [address, setAddress] = useState('')
     const cartStore = useCartStore()
+    const navigation = useNavigation()
+
     const total = formatCurrency(cartStore.products.reduce((total, product) => total + 
     (product.price * product.quantity), 0))
+
+    function handleProductRemove(product: ProductCartProps) {
+        Alert.alert("Remover", `Deseja remover ${product.title} do carrinho?`, [
+            {
+                text: 'Cancelar',
+            },
+            {
+                text: 'Remover',
+                onPress: () =>{
+                    cartStore.remove(product.id)
+                } ,
+
+            }
+        ])
+    }
+
+    function handleOrder() {
+        if (address.trim().length === 0){
+            return Alert.alert('Pedido', 'Informe os dados da entrega.')
+        }
+
+        const products = cartStore.products.map((product) => `\n ${product.quantity} ${product.title}`).join('')
+
+        const message = `
+        🍔NOVO PEDIDO
+        \n Entregar em: ${address}
+
+        ${products}
+
+        \n Valor total: ${total}
+        `
+
+        Linking.openURL(`http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`)
+
+        handleCartEmpty()
+    }
+
+    function handleCartEmpty() {
+        cartStore.clear()
+        navigation.goBack()
+    }
+
     return(
         <View className="flex-1 pt-8" >
             <Header title="Seu carrinho" />
@@ -24,7 +73,11 @@ export default function Cart() {
                             <View className="border-b border-slate-700">    
                                 {
                                     cartStore.products.map((product) => (
-                                        <Product key={product.id} data={product} />
+                                        <Product 
+                                            key={product.id} 
+                                            data={product} 
+                                            onPress={() => handleProductRemove(product)} 
+                                        />
                                     ))
                                 }
                             </View>
@@ -40,13 +93,19 @@ export default function Cart() {
                             <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
                         </View>
 
-                        <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
+                        <Input 
+                            placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." 
+                            onChangeText={setAddress}
+                            onSubmitEditing={handleOrder}
+                            blurOnSubmit
+                            returnKeyType="send"
+                        />
                     </View>
                 </ScrollView>
             </KeyboardAwareScrollView>
 
             <View className="p-5 gap-5" >
-                <Button>
+                <Button onPress={handleOrder}>
                     <Button.Text>Enviar pedido</Button.Text>
                     <Button.Icon>
                         <Feather name="arrow-right-circle" size={20} />
